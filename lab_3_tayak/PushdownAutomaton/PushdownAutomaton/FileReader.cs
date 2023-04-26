2324afb30e5fbb27dda04563e56dd95f739241e1
Vladislavhgtech <51228868+Vladislavhgtech@users.noreply.github.com>
2023-04-26 19:56:17 +0300

    Lab 3

---
+++ lab_3_tayak/PushdownAutomaton/PushdownAutomaton/FileReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushdownAutomaton
+{
+    public static class FileReader
+    {
+        public static string[] Read()
+        {
+            string text;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("../../Files/text1.txt", System.Text.Encoding.Default))
+                {
+                    text = sr.ReadToEnd();
+                }
+
+                return text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
